7bc3aca7715c35d94bc27595a292cdabb3cd6d30
NITSAN Technologies <sanjay@nitsan.in>
2020-04-02 13:18:59 +0530

    [BUGFIX] Improved TYPO3 v10 Backend Module Design Compatiblity

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,7 +1,7 @@
 <?php
 
 $EM_CONF[$_EXTKEY] = [
-    'title' => '[NITSAN] Backup',
+    'title' => '[NITSAN] Backup Plus',
     'description' => 'First-ever feature-rich TYPO3 backup solution with manual and scheduled backup. Easy to use and configure. Backup your TYPO3 code, assets, database etc to your favourite backup clouds/servers. Demo-Screecasts: http://demo.t3terminal.com/t3t-extensions/backup/ You can buy PRO-version to get more features and free-support at https://t3terminal.com/ns-backup-typo3-extension',
     'category' => 'module',
     'author' => 'NITSAN Technologies Pvt Ltd',
