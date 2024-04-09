f366911f61e5dd4bb2061628decffdf83391ffb3
NITSAN <sanjay@nitsan.in>
2024-04-09 18:39:14 +0530

    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -2,7 +2,9 @@
 
 $EM_CONF['ns_backup'] = [
     'title' => '[NITSAN] Backup Plus',
-    'description' => 'First-ever feature-rich TYPO3 backup solution with manual and scheduled backup. Easy to use and configure. Backup your TYPO3 code, assets, database etc to your favourite backup clouds/servers. Demo-Screecasts: http://demo.t3planet.com/t3t-extensions/backup/ You can buy PRO-version to get more features and free-support at https://t3planet.com/ns-backup-typo3-extension',
+    'description' => 'Easily backup your entire TYPO3 website. The Backup Plus extension for TYPO3 lets you save your code, files, and database with just a few clicks. Install Backup Plus and connect it to your cloud storage (like Google Drive, Dropbox, Amazon S3, SFTP, Rsync, etc.) and backup your entire TYPO3 site manually with a single click.
+
+*** Live Demo: https://demo.t3planet.com/t3-extensions/backup *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-backup-extension',
     'category' => 'module',
     'author' => 'T3D: Rohan Parmar, Divya Goklani, Nilesh Malankiya, QA: Gautam Kunjadiya',
     'author_email' => 'sanjay@nitsan.in',
