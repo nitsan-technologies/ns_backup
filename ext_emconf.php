a0b12778ee42d5a300b49d7fb478c4edff2ffcad
NITSAN <sanjay@nitsan.in>
2025-06-24 13:53:31 +0530

    Update ext_emconf.php
    Update ext_emconf.php

--- ext_emconf.php
+++ ext_emconf.php
@@ -1,14 +1,13 @@
 <?php
 
 $EM_CONF['ns_backup'] = [
-    'title' => 'Backup Plus',
-    'description' => 'Easily backup your entire TYPO3 website. The Backup Plus extension for TYPO3 lets you save your code, files, and database with just a few clicks. Install Backup Plus and connect it to your cloud storage (like Google Drive, Dropbox, Amazon S3, SFTP, Rsync, etc.) and backup your entire TYPO3 site manually with a single click.
+    'title' => 'TYPO3 Backup Plus',
+    'description' => 'Easily back up your entire TYPO3 site—including code, files, and database—with one click. Supports cloud storage like Google Drive, Dropbox, Amazon S3, SFTP, Rsync, and more.',
 
-    *** Live Demo: https://demo.t3planet.com/t3-extensions/backup *** Premium Version, Documentation & Free Support: https://t3planet.com/typo3-backup-extension',
     'category' => 'module',
-    'author' => 'T3: Rohan Parmar, T3: Divya Goklani, T3: Nilesh Malankiya, QA: Krishna Dhapa',
-    'author_email' => 'sanjay@nitsan.in',
-    'author_company' => 'T3Planet // NITSAN',
+    'author' => 'Team T3Planet',
+    'author_email' => 'info@t3planet.de',
+    'author_company' => 'T3Planet',
     'state' => 'stable',
     'uploadfolder' => 1,
     'createDirs' => '',
